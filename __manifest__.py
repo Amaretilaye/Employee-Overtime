dc5e669e79288add0508c9d5e279e2c40732a117
Amaretilaye <114220240+Amaretilaye@users.noreply.github.com>
2024-10-01 08:39:27 -0700

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,7 @@
     'summary': 'employee overtime in odoo based on working hour and over time type it is configerable by any company rule',
     'description': """this module requires odoo mate payroll module """,
     'category': '',
+    'aouther':'Amare Tilaye,
     'website': '',
     'depends': [
         'hr',
